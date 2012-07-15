129a5a0aa5a6c73c3789ae17abe3127d2f6e83ed
johannsutherland <johannsutherland@yahoo.co.uk>
2012-07-15 17:54:49 +0100

    Completed Problem 6

---
+++ Problem6/Problem6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int upperLimit = 100;
+            int squareOfSums = (int)Math.Pow(Enumerable.Range(1, upperLimit).Aggregate((y, x) => y + x), 2);
+            int sumOfSquares = Enumerable.Range(1, upperLimit).Aggregate((y, x) => y + (int)Math.Pow(x, 2));
+            Console.WriteLine(squareOfSums - sumOfSquares);
+            Console.ReadKey();
+        }
+    }
+}
