e7e5db3d72ec5dfe152ae316c5c446cc130dd947
johannsutherland <johannsutherland@yahoo.co.uk>
2012-07-15 17:40:21 +0100

    Completed Problem 5

---
+++ Problem5/Problem5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lastNumber = 20;
+
+            int counter = lastNumber;
+            int currentNumber = lastNumber;
+            bool found = false;
+
+            while (!found)
+            {
+                int testNumber = currentNumber * counter;
+                bool foundFailedDivisor = false;
+
+                for (int trailDivisor = lastNumber - 1; trailDivisor > 1; trailDivisor--)
+                {
+                    if (testNumber % trailDivisor != 0)
+                    {
+                        foundFailedDivisor = true;
+                        break;
+                    }
+                }
+
+                if (!foundFailedDivisor)
+                {
+                    Console.WriteLine(testNumber);
+                    found = true;
+                }
+
+                counter++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
