240083c581eae5a608915149e311e3131cfdc303
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-03-30 23:40:56 +0100

    Completed Problem 30

---
+++ Problem30/FactorCheckerUnitTest/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem30;
+using System.Collections.Generic;
+
+namespace FactorCheckerUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetDigitsFor1234()
+        {
+            int number = 1234;
+            int expected = 4321;
+            int result = 0;
+            foreach (int d in FactorChecker.GetDigits(number))
+            {
+                result = result * 10 + d;
+            }
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Example1634()
+        {
+            int number = 1634;
+            int power = 4;
+
+            Assert.AreEqual(number, FactorChecker.RaiseDigitsToPower(number, power));
+        }
+
+        [TestMethod]
+        public void Example1635()
+        {
+            int number = 1635;
+            int power = 4;
+
+            Assert.AreNotEqual(number, FactorChecker.RaiseDigitsToPower(number, power));
+        }
+
+        [TestMethod]
+        public void Example8208()
+        {
+            int number = 8208;
+            int power = 4;
+
+            Assert.AreEqual(number, FactorChecker.RaiseDigitsToPower(number, power));
+        }
+
+        [TestMethod]
+        public void Example9474()
+        {
+            int number = 9474;
+            int power = 4;
+
+            Assert.AreEqual(number, FactorChecker.RaiseDigitsToPower(number, power));
+        }
+    }
+}
---
+++ Problem30/Problem30/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            double max = 6 * Math.Pow(9, 5);
+            for (int i = 2; i < max; i++)
+            {
+                if (i == FactorChecker.RaiseDigitsToPower(i, 5))
+                {
+                    total += i;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Total: " + total);
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+
+    public class FactorChecker
+    {
+        public static IEnumerable<int> GetDigits(int number)
+        {
+            int digit = number % 10;
+            int remainder = number / 10;
+
+            yield return digit;
+
+            while (remainder != 0)
+            {
+                digit = remainder % 10;
+                remainder = remainder / 10;
+
+                yield return digit;
+            }
+        }
+
+        public static double RaiseDigitsToPower(int number, int power)
+        {
+            double result = 0;
+            foreach (int digit in GetDigits(number))
+            {
+                result += Math.Pow(digit, power);
+            }
+            return result;
+        }
+    }
+}
