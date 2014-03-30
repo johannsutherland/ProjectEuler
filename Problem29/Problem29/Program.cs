3fb43c38a6e92e4dd3bbedc97e4f900d2c143043
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-03-30 23:05:26 +0100

    Completed Problem 29

---
+++ Problem29/Problem29/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = new List<double>();
+
+            for (int a = 2; a <= 100; a++)
+            {
+                for (int b = 2; b <= 100; b++)
+                {
+                    numbers.Add(Math.Pow(a, b));
+                }
+            }
+
+            Console.WriteLine(numbers.Distinct().Count());
+            Console.ReadKey();
+        }
+    }
+}
