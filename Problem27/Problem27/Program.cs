34e4be45e1f54c2cfbea93ec1735a01bef9ca1a2
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-03-30 21:00:08 +0100

    Completed Problem 27

---
+++ Problem27/Problem27/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primes p = new Primes();
+            int max = 0;
+
+            for (int a = -1000; a <= 1000; a++)
+            {
+                for (int b = -1000; b <= 1000; b++)
+                {
+                    QuadricPolynominal qp = new QuadricPolynominal(a, b, p);
+                    int length = qp.FindLength();
+                    if (length > max)
+                    {
+                        max = length;
+                        Console.WriteLine("n2 + {0}n + {1}, length = {2}, product of coefficients = {3}", a, b, length, a * b);
+                    }
+                }
+            }
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+
+    public class QuadricPolynominal
+    {
+        private int a;
+        private int b;
+        private Primes primes;
+
+        public QuadricPolynominal(int a, int b, Primes primes)
+        {
+            this.a = a;
+            this.b = b;
+            this.primes = primes;
+        }
+
+        public double Solve(int n)
+        {
+            return Math.Pow(n, 2) + this.a * n + this.b;
+        }
+
+        public int FindLength()
+        {
+            bool allPrime = true;
+            int length = 0;
+
+            while (allPrime)
+            {
+                double answer = this.Solve(length);
+                allPrime = allPrime && primes.IsPrime(answer);
+                if (!allPrime)
+                {
+                    break;
+                }
+                length++;
+
+                if (length % 100 == 0)
+                    Console.ReadKey();
+            }
+            return length;
+        }
+    }
+
+    public class Primes
+    {
+        public List<double> ExistingPrimes;
+
+        public Primes()
+        {
+            ExistingPrimes = new List<double>();
+        }
+
+        public bool IsPrime(double candidate)
+        {
+            if (candidate <= 1)
+                return false;
+
+            if (ExistingPrimes.Contains(candidate))
+                return true;
+
+            double testingUpperLimit = Math.Round(Math.Sqrt(candidate));
+
+            bool isPrime = true;
+            foreach (double prime in ExistingPrimes)
+            {
+                if (prime > testingUpperLimit)
+                    break;
+
+                if (candidate % prime == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                for (double i = 2; i <= testingUpperLimit; i++)
+                {
+                    if (candidate % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime) ExistingPrimes.Add(candidate);
+
+            return isPrime;
+        }
+    }
+}
---
+++ Problem27/QuadricPolynominalTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem27;
+
+namespace QuadricPolynominalTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            Primes p = new Primes();
+
+            QuadricPolynominal qp = new QuadricPolynominal(1, 41, p);
+            Assert.AreEqual(40, qp.FindLength());
+        }
+
+        [TestMethod]
+        public void Example2()
+        {
+            Primes p = new Primes();
+
+            QuadricPolynominal qp = new QuadricPolynominal(-79, 1601, p);
+            Assert.AreEqual(80, qp.FindLength());
+        }
+
+        [TestMethod]
+        public void Is1681Prime()
+        {
+            Primes p = new Primes();
+            double number = 1681;
+            Assert.IsFalse(p.IsPrime(number));
+        }
+    }
+}
