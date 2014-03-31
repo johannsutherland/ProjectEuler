ee0d5550b00c086eb3a8cd9ecf9b9a39fa35460a
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-03-31 18:56:45 +0100

    Completed Problem 32

---
+++ Problem32/PandigitalTest/PandigitalTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem32;
+
+namespace PandigitalTest
+{
+    [TestClass]
+    public class PandigitalTest
+    {
+        [TestMethod]
+        public void Num1234IsPandigital()
+        {
+            Assert.IsTrue(Pandigital.IsPandigital(1234));
+        }
+
+        [TestMethod]
+        public void Num9825IsPandigital()
+        {
+            Assert.IsTrue(Pandigital.IsPandigital(9825));
+        }
+
+        [TestMethod]
+        public void Num9885IsNotPandigital()
+        {
+            Assert.IsFalse(Pandigital.IsPandigital(9885));
+        }
+
+        [TestMethod]
+        public void Num1234And5678And9IsPandigital()
+        {
+            Assert.IsTrue(Pandigital.IsPandigital(1234, 5678, 9));
+        }
+
+        [TestMethod]
+        public void Num9825And134And67IsPandigital()
+        {
+            Assert.IsTrue(Pandigital.IsPandigital(9825, 134, 67));
+        }
+
+        [TestMethod]
+        public void Num985And345IsNotPandigital()
+        {
+            Assert.IsFalse(Pandigital.IsPandigital(985, 345, 123));
+        }
+    }
+}
---
+++ Problem32/Problem32/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> products = new List<int>();
+
+            for (int a = 1; a < 100; a++)
+            {
+                for (int b = 100; b < 10000; b++)
+                {
+                    int product = a * b;
+                    if (Pandigital.IsPandigital(a, b, product))
+                    {
+                        Console.WriteLine("{0} x {1} = {2}", a, b, product);
+                        if (!products.Contains(a * b)) products.Add(product);
+                    }
+                }
+            }
+
+            Console.WriteLine(products.Sum());
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+
+    public class Pandigital
+    {
+        public static bool IsPandigital(long number)
+        {
+            string num = number.ToString();
+            if (num.Contains('0'))
+                return false;
+
+            int[] usedDigits = new int[10];
+
+            while (num.Length > 0)
+            {
+                int digit = int.Parse(num[0].ToString());
+                if (usedDigits[digit] > 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    num = num.Substring(1);
+                    usedDigits[digit]++;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsPandigital(int number1, int number2, int number3)
+        {
+            string bigNumber = number1.ToString() + number2.ToString() + number3.ToString();
+
+            if (bigNumber.Length != 9)
+                return false;
+
+            if (bigNumber.Contains('0'))
+                return false;
+
+            long concatenatedNumber = long.Parse(bigNumber.ToString());
+
+            return IsPandigital(concatenatedNumber);
+        }
+    }
+}
