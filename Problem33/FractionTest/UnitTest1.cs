d731425acccd9404e50390fe1c632b65b80f0231
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-04-07 22:43:43 +0100

    Completed Problem 33

---
+++ Problem33/FractionTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem33;
+
+namespace FractionTests
+{
+    [TestClass]
+    public class FractionTest
+    {
+        [TestMethod]
+        public void TestValueIsDecimal()
+        {
+            Fraction f = new Fraction(1, 2);
+            Assert.AreEqual(0.5M, f.Value());
+        }
+
+        [TestMethod]
+        public void TestValueIsRepeatingDecimal()
+        {
+            Fraction f = new Fraction(1, 3);
+            Assert.AreEqual(1M / 3M, f.Value());
+        }
+
+        [TestMethod]
+        public void HasIdenticalDigit()
+        {
+            Fraction f = new Fraction(13, 43);
+            Assert.AreEqual('3', f.HasIdenticalDigit());
+        }
+
+        [TestMethod]
+        public void NoIdenticalDigit()
+        {
+            Fraction f = new Fraction(15, 43);
+            Assert.AreEqual(null, f.HasIdenticalDigit());
+        }
+
+        [TestMethod]
+        public void NoIdenticalDigit2()
+        {
+            Fraction f = new Fraction(94, 11);
+            Assert.AreEqual(null, f.HasIdenticalDigit());
+        }
+
+        [TestMethod]
+        public void RemoveDigit()
+        {
+            Fraction f = new Fraction(13, 43);
+            Fraction newf = f.RemoveDigit('3');
+            Assert.AreEqual(1, newf.Nominator);
+            Assert.AreEqual(4, newf.Denominator);
+        }
+
+        [TestMethod]
+        public void Equality()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(2, 4);
+            Assert.IsTrue(f1.Equals(f2));
+        }
+    }
+}
---
+++ Problem33/Problem33/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem33
+{
+    public class Fraction : IEquatable<Fraction>
+    {
+        public int Nominator { get; set; }
+        public int Denominator { get; set; }
+
+        public Fraction(int Nominator, int Denominator)
+        {
+            this.Nominator = Nominator;
+            this.Denominator = Denominator;
+        }
+
+        public decimal Value()
+        {
+            if (this.Denominator != 0)
+                return (decimal)this.Nominator / (decimal)this.Denominator;
+            else
+                return decimal.MaxValue;
+        }
+
+        public char? HasIdenticalDigit()
+        {
+            string nominator = this.Nominator.ToString();
+            foreach (char c in nominator)
+            {
+                if (nominator.Where(s => s == c).Count() > 1)
+                    continue;
+
+                if (this.Denominator.ToString().Where(s => s == c).Count() == 1)
+                    return c;
+            }
+
+            return null;
+        }
+
+        public Fraction RemoveDigit(char c)
+        {
+            return new Fraction(int.Parse(this.Nominator.ToString().Replace(c.ToString(), String.Empty)),
+                int.Parse(this.Denominator.ToString().Replace(c.ToString(), String.Empty)));
+        }
+
+        public override string ToString()
+        {
+            return this.Nominator + "/" + this.Denominator;
+        }
+
+        public bool Equals(Fraction other)
+        {
+            return this.Value() == other.Value();
+        }
+
+        public bool IsValid()
+        {
+            return !((this.Denominator == this.Nominator) || (this.Denominator % 10 == 0) || (this.Nominator % 10 == 0));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fraction> fractions = new List<Fraction>();
+
+            for (int n = 10; n < 100; n++)
+            {
+                for (int d = 10; d < 100; d++)
+                {
+                    Fraction f = new Fraction(n, d);
+
+                    if (!f.IsValid())
+                        continue;
+
+                    char? c = f.HasIdenticalDigit();
+                    if (c.HasValue)
+                    {
+                        Fraction newF = f.RemoveDigit(c.Value);
+                        if (f.Equals(newF) && newF.Value() < 1)
+                        {
+                            Console.WriteLine(f);
+                            fractions.Add(f);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(fractions.Aggregate(1M, (product, f) => product *= f.Value()));
+
+            Console.ReadKey();
+        }
+    }
+}
