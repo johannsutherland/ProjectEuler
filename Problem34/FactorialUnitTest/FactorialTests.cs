3664dd52c353429b71cdcedd3b56cb122af9b7db
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-04-10 23:45:29 +0100

    Completed Problem 34

---
+++ Problem34/FactorialUnitTest/FactorialTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem34;
+using System.Collections.Generic;
+
+namespace FactorialUnitTest
+{
+    [TestClass]
+    public class FactorialTests
+    {
+        [TestMethod]
+        public void GetDigits()
+        {
+            int[] digits = new int[4];
+            int i = 3;
+            int[] expected = { 1, 2, 3, 4 };
+            foreach (int d in DigitsManager.GetDigits(1234))
+            {
+                digits[i] = d;
+                Assert.AreEqual(digits[i], expected[i]);
+                i--;
+            }
+        }
+
+        [TestMethod]
+        public void CalculateFactorialOf1()
+        {
+            Factorial f = new Factorial();
+            Assert.AreEqual(1, f.CalculateFactorial(1));
+        }
+
+        [TestMethod]
+        public void CalculateFactorialOf3()
+        {
+            Factorial f = new Factorial();
+            Assert.AreEqual(6, f.CalculateFactorial(3));
+        }
+
+        [TestMethod]
+        public void CalculateFactorialOf5()
+        {
+            Factorial f = new Factorial();
+            Assert.AreEqual(120, f.CalculateFactorial(5));
+        }
+
+        [TestMethod]
+        public void CalculateFactorialOf20()
+        {
+            Factorial f = new Factorial();
+            Assert.AreEqual(2432902008176640000, f.CalculateFactorial(20));
+        }
+
+        [TestMethod]
+        public void CalculateFactorialOf3WithCache()
+        {
+            Factorial f = new Factorial();
+            Assert.AreEqual(6, f.CalculateFactorial(3));
+            Assert.AreEqual(6, f.CalculateFactorial(3));
+        }
+    }
+}
---
+++ Problem34/Problem34/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Problem34
+{
+    class Program
+    {
+        public const int UpperBound = 2540160;
+
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            Factorial f = new Factorial();
+            long sum = 0;
+
+            for (int n = 3; n < UpperBound; n++)
+            {
+                long total = 0;
+                long remainder = n;
+                while (remainder > 0)
+                {
+                    long digit = remainder % 10;
+                    total += f.CalculateFactorial(digit);
+                    remainder /= 10;
+                }
+
+                if (total == n)
+                {
+                    sum += total;
+                    Console.WriteLine("Found {0}", n);
+                }
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Sum {0} in {1}", sum, sw.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+
+    public class DigitsManager
+    {
+        public static IEnumerable<int> GetDigits(int n)
+        {
+            int remainder = n;
+            while (remainder > 0)
+            {
+                yield return remainder % 10;
+                remainder /= 10;
+            }
+        }
+    }
+
+    public class Factorial
+    {
+        long[] cache;
+
+        public Factorial()
+        {
+            cache = new long[Problem34.Program.UpperBound];
+        }
+
+        public long CalculateFactorial(long n)
+        {
+            if (n <= 1)
+                return 1;
+
+            if (cache[n] != 0) return cache[n];
+
+            if (n < 1)
+                throw new Exception("Expecting n > 0");
+
+            long factorial = 1;
+
+            for (long i = n; i > 1; i--)
+            {
+                factorial *= i;
+            }
+
+            cache[n] = factorial;
+            return factorial;
+        }
+    }
+}
