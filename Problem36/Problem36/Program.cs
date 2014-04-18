0cc2d76ec204407f7f7c72fff568597b93daf4f5
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-04-18 20:11:02 +0100

    Completed Problem 36

---
+++ Problem36/Problem36/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = 1000000;
+            int sumOfPalindromes = 0;
+
+            for (int number = 1; number < max; number++)
+            {
+                if (isPalindrome(number) && isPalindrome(Convert.ToString(number, 2)))
+                {
+                    Console.WriteLine(number);
+                    sumOfPalindromes += number;
+                }
+            }
+            Console.WriteLine("The sum is {0}", sumOfPalindromes);
+            Console.ReadKey();
+        }
+
+        static bool isPalindrome(string number)
+        {
+            int boundary = (int)Math.Ceiling((double)number.Length / 2);
+            for (int i = 0; i < boundary; i++)
+            {
+                if (number[i] != number[number.Length - 1 - i])
+                    return false;
+            }
+            return true;
+        }
+
+        static bool isPalindrome(long number)
+        {
+            return isPalindrome(number.ToString());
+        }
+    }
+}
