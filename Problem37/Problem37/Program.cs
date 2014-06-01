e0fb3b53b5c8081c3ab3afd706f8141b0c71be00
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-06-01 11:44:05 +0100

    Completed Problem 37

---
+++ Problem37/Problem37/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem37
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Primes p = new Primes();
+
+            List<int> foundPrimes = new List<int>();
+            List<int> digits = Enumerable.Range(1, 9).ToList();
+
+            List<int> thisGeneration = new List<int> { 2, 3, 5, 7 };
+            List<int> nextGeneration = new List<int>();
+
+            while (foundPrimes.Count() < 11)
+            {
+                foreach (int number in thisGeneration)
+                {
+                    foreach (int numberWithDigit in AddDigit(number, digits, Side.Left))
+                    {
+                        if (p.isPrime(numberWithDigit))
+                        {
+                            nextGeneration.Add(numberWithDigit);
+
+                            bool isPrime = true;
+                            foreach (int numberWithoutDigit in RemoveDigit(numberWithDigit, Side.Right))
+                            {
+                                isPrime = isPrime && p.isPrime(numberWithoutDigit);
+
+                                if (!isPrime)
+                                {
+                                    break;
+                                }
+                            }
+
+                            if (isPrime)
+                            {
+                                foundPrimes.Add(numberWithDigit);
+                            }
+                        }
+                    }
+                }
+
+                thisGeneration.Clear();
+                thisGeneration.AddRange(nextGeneration);
+                nextGeneration.Clear();
+            }
+
+            foundPrimes.Sort();
+            foreach (long number in foundPrimes)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("Total: {0}", foundPrimes.Sum());
+            
+            Console.ReadKey();
+        }
+
+        public enum Side { Left, Right }
+
+        public static IEnumerable<long> AddDigit(int number, List<int> digits, Side side)
+        {
+            foreach (int digit in digits)
+            {
+                if (side == Side.Right)
+                    yield return number * 10 + digit;
+                else
+                    yield return digit * (int)Math.Pow(10, 1 + Math.Floor(Math.Log10(number))) + number;
+            }
+        }
+
+        public static IEnumerable<int> RemoveDigit(int numberToRemove, Side side)
+        {
+            int number = numberToRemove;
+            while (number > 10)
+            {
+                if (side == Side.Right)
+                {
+                    int digit = number % 10;
+                    number /= 10;
+                    yield return number;
+                }
+                else
+                {
+                    int divider = (int)Math.Pow(10, Math.Floor(Math.Log10(number)));
+                    int digit = (int)Math.Floor((double)(number / divider));
+                    number %= divider;
+                    yield return number;
+                }
+            }
+        }
+    }
+
+    class Primes
+    {
+        public SortedSet<long> ExistingPrimes = new SortedSet<long>();
+
+        private long currentMaximum = 1;
+
+        public Primes(int maximumNumber = 0)
+        {
+            isPrime(maximumNumber);
+        }
+
+        public bool isPrime(long candidate)
+        {
+            if (candidate <= 1)
+            {
+                return false;
+            }
+
+            if (currentMaximum >= candidate)
+            {
+                return ExistingPrimes.Contains(candidate);
+            }
+
+            for (long tempCandidate = currentMaximum + 1; tempCandidate < candidate; tempCandidate++)
+            {
+                CheckPrime(tempCandidate);
+            }
+
+            return CheckPrime(candidate);
+        }
+
+        private bool CheckPrime(long candidate)
+        {
+            bool isPrime = true;
+            long testingUpperLimit = (long)Math.Round(Math.Sqrt(candidate));
+
+            foreach (long prime in ExistingPrimes)
+            {
+                if (prime > testingUpperLimit)
+                {
+                    break;
+                }
+
+                if (candidate % prime == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (currentMaximum < candidate)
+            {
+                currentMaximum = candidate;
+            }
+
+            if (isPrime)
+            {
+                ExistingPrimes.Add(candidate);
+            }
+
+            return isPrime;
+        }
+    }
+}
---
+++ Problem37/UnitTest/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem37;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void RemoveDigitFromLeft()
+        {
+            List<int> digits = new List<int>();
+            int number = 123456;
+            List<int> expectedDigits = new List<int> { 23456, 3456, 456, 56, 6 };
+
+            foreach (int digit in Program.RemoveDigit(number, Program.Side.Left))
+            {
+                digits.Add(digit);
+            }
+
+            Assert.IsTrue(digits.SequenceEqual(expectedDigits));
+        }
+
+        [TestMethod]
+        public void RemoveDigitFromRight()
+        {
+            List<int> digits = new List<int>();
+            int number = 123456;
+            List<int> expectedDigits = new List<int> { 12345, 1234, 123, 12, 1 };
+
+            foreach (int digit in Program.RemoveDigit(number, Program.Side.Right))
+            {
+                digits.Add(digit);
+            }
+
+            Assert.IsTrue(digits.SequenceEqual(expectedDigits));
+        }
+
+        [TestMethod]
+        public void AddDigitToRight()
+        {
+            List<int> digits = new List<int> { 2, 3, 4, 5, 6 };
+            int startNumber = 1;
+            List<int> expectedNumbers = new List<int> { 12, 13, 14, 15, 16 };
+            List<int> numbers = new List<int>();
+
+            foreach (int number in Program.AddDigit(startNumber, digits, Program.Side.Right))
+            {
+                numbers.Add(number);
+            }
+
+            Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+        }
+
+        [TestMethod]
+        public void AddDigitToLeft()
+        {
+            List<int> digits = new List<int> { 2, 3, 4, 5, 6 };
+            int startNumber = 1;
+            List<int> expectedNumbers = new List<int> { 21, 31, 41, 51, 61 };
+            List<int> numbers = new List<int>();
+
+            foreach (int number in Program.AddDigit(startNumber, digits, Program.Side.Left))
+            {
+                numbers.Add(number);
+            }
+
+            Assert.IsTrue(numbers.SequenceEqual(expectedNumbers));
+        }
+    }
+}
