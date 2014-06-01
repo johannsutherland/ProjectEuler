4dd4d14a65bdb7d19dd29fc70f2f359fa086ea6f
Johann Sutherland <johannsutherland@yahoo.co.uk>
2014-06-01 16:47:09 +0100

    Completed Problem 39

---
+++ Problem39/Problem39/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pLimit = 1000;
+            int[] solutions = new int[pLimit];
+            int maxSolutions = 0;
+            int maxP = 0;
+
+            for (int a = 1; a < pLimit / 2; a++)
+            {
+                for (int b = a; a + b < pLimit; b++)
+                {
+                    double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+                    if ((Math.Floor(c) == c))
+                    {
+                        int p = a + b + (int)c;
+                        if (p < pLimit)
+                        {
+                            solutions[p]++;
+                            if (solutions[p] > maxSolutions)
+                            {
+                                maxSolutions = solutions[p];
+                                maxP = p;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("p = {0} with {1} solutions,", maxP, maxSolutions);
+            Console.ReadKey();
+        }
+    }
+}
